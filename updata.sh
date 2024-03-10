b8ab1c1c7b866cbabfe3a25ab579296503d92e19
Qmm <1259598502@qq.com>
2024-03-10 12:51:15 +0800

    :pencil: update content

--- updata.sh
+++ updata.sh
@@ -2,7 +2,8 @@
 # 如果没有消息后缀，默认提交信息为 `:pencil: update content`
 info=$1
 if ["$info" = ""];
-then info=":pencil: update content"
+then
+    info=":pencil: update content"
 fi
 git add -A
 git commit -m "$info"
